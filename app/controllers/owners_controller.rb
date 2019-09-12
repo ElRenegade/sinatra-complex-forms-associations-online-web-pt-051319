d1bfc3835ffe392f434b0402cba27f1fbbebb2ca
Santiago Sanchez <chagostwo@yahoo.com>
2019-09-12 02:16:38 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -14,11 +14,16 @@ class OwnersController < ApplicationController
     @owner = Owner.create(params[:owner])
     if !params["pet"]["name"].empty?
       @owner.pets << Pet.create(name: params["pet"]["name"])
+<<<<<<< HEAD
       # When using the shovel operator, ActiveRecord instantly fires update SQL
       # without waiting for the save or update call on the parent object,
       # unless the parent object is a new record.
     end
     redirect "/owners/#{@owner.id}"
+=======
+    end
+      redirect "owners/#{@owner.id}"
+>>>>>>> f95cd04434b62cffee5795de08bc32934f3a6360
   end
 
   get '/owners/:id/edit' do
@@ -33,6 +38,7 @@ class OwnersController < ApplicationController
   end
 
   patch '/owners/:id' do
+<<<<<<< HEAD
     @owner = Owner.find(params[:id])
     @owner.update(params[:owner])
 
@@ -42,4 +48,18 @@ class OwnersController < ApplicationController
 
     redirect "/owners/#{@owner.id}"
   end
+=======
+    if !params[:owner].keys.include?("pet_ids")
+      params[:owner]["pet_ids"] = []
+    end
+    #######
+
+    @owner = Owner.find(params[:id])
+    @owner.update(params["owner"])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+      redirect "owners/#{@owner.id}"
+    end
+>>>>>>> f95cd04434b62cffee5795de08bc32934f3a6360
 end
